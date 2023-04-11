e861b875876519386978588c2ef46138e54df597
Anka <103377387+ann153476@users.noreply.github.com>
2023-04-11 15:20:36 +0300

    іп

---
+++ src/components/MyComponent/MyComponent.jsx
@@ -0,0 +1,12 @@
+import s from './my_component.module.scss';
+export const MyComponent = () => {
+  return (
+    <>
+      <h1 className={s.red}>MyComponent надеюсь єтот буде работать</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        cum a quos fugiat quibusdam alias deserunt quia minima, ex illum!
+      </p>
+    </>
+  );
+};
