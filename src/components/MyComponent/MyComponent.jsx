66be8e61641e78af3952935c1f8f90079a488678
Anka <103377387+ann153476@users.noreply.github.com>
2023-04-11 15:34:18 +0300

    all

--- src/components/MyComponent/MyComponent.jsx
+++ src/components/MyComponent/MyComponent.jsx
@@ -3,10 +3,22 @@ export const MyComponent = () => {
   return (
     <>
       <h1 className={s.red}>MyComponent надеюсь єтот буде работать</h1>
+      <p>создать проэкт на git hub</p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        cum a quos fugiat quibusdam alias deserunt quia minima, ex illum!
+        выбрать Settings Actions General в последнем абзаце парвый пункт выбрат
+        и поставить галочку{' '}
       </p>
+      <p>склонировать репозиторий себе на комп</p>
+      <p>
+        перенести все файлы из этого репозитория в новый кроме папки
+        node_modules
+      </p>
+      <p>в файле package.json изменить имя репозитория на новое</p>
+      <p>npm install npm start</p>
+      <p>пушим теперь</p>
+      <p>ждём пока пройдет деплой</p>
+      <p>Settings pages gh-pages save</p>
+      <p>появляеться ссылка на сайт теперь можно редактировать и сайт готов</p>
     </>
   );
 };
